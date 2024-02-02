ac6d30b3087d1d1853e90e083c12c250007d55a1
PierreMorin4590 <pierremorin4590@hotmail.fr>
2024-02-03 00:01:19 +0100

    app.js copy/paste :white_check_mark:

--- app.js
+++ app.js
@@ -4,19 +4,36 @@ const app = {
 
     init: function() {
         console.log('app.init()');
-        this.selectors = document.querySelectorAll(".selector");
-        this.selectors.addEventListener("click", (event) => handleCopyContent(event));
-        
+
+        // On sélectionne tous les sélécteurs
+        const selectors = document.querySelectorAll(".selector");
+
+        // On place un addEventListener sur chacun des sélecteurs
+        selectors.forEach(selector => {
+            selector.addEventListener("click", this.handleCopyContent);
+        });
+
+        // On sélectionne toutes les cellules
+        const cells = document.querySelectorAll(".grid");
+
+        // On place un addEventListener sur chacune des cellules
+        cells.forEach(cell => {
+            cell.addEventListener("click", this.handlePasteContent);
+        });
+
     },
 
-    handleCopyContent: function(sourceNumber) {
+    handleCopyContent: function (event) {
+        console.log("J'ai copié !");
+
+        const sourceNumber = event.currentTarget.textContent;
+
         // Construisez la classe de source en fonction du numéro choisi
-        const sourceClass = '.selector:nth-child(' + sourceNumber + ')';
+        var sourceClass = '.selector:nth-child(' + sourceNumber + ')';
         console.log(sourceClass);
 
         // Sélectionnez la source par sa classe
         var sourceDiv = document.querySelector(sourceClass);
-        console.log(sourceDiv);
 
         // Vérifiez si l'élément source est présent
         if (sourceDiv) {
@@ -26,9 +43,25 @@ const app = {
         }
     },
 
-    
+    handlePasteContent: function(event) {
+        console.log("J'ai collé !");
 
-    
+        const destinationNumber = event.currentTarget.classList[1];
+
+        // Construisez la classe de destination en fonction du numéro choisi
+        var destinationClass = '.' + destinationNumber;
+        console.log(destinationClass);
+
+        // Sélectionnez la destination par sa classe
+        var destinationDiv = document.querySelector(destinationClass);
+        console.log(destinationDiv);
+
+        // Vérifiez si l'élément destination est présent et s'il y a du contenu à coller
+        if (destinationDiv && contentToCopy !== "") {
+            // Collez le contenu dans la destination
+            destinationDiv.innerHTML = contentToCopy;
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => app.init());
